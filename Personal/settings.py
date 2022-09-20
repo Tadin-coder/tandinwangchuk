40c55c505a1e4fdc2a99f1c111fd4ea6b403f38b
Tandin Wangchuk <12190027.gcit@rub.edu.bt>
2022-09-20 19:56:02 +0600

    4

--- Personal/settings.py
+++ Personal/settings.py
@@ -131,4 +131,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR / 'static/images')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
