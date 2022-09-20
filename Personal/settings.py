4f4497e5933ebe81bbdaba7dad048363ec5b2ce4
Tandin Wangchuk <12190027.gcit@rub.edu.bt>
2022-09-20 19:53:05 +0600

    4

--- Personal/settings.py
+++ Personal/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/assets/static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRLS = [
@@ -131,4 +131,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR / 'static/images')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
